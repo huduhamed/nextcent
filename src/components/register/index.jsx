02d9d76b39bf5c5296297d1f9a848da91b690e37
Hudu <92208883+huduhamed@users.noreply.github.com>
2026-08-04 23:44:46 +0200

    refactor form UI (#6)

--- src/components/register/index.jsx
+++ src/components/register/index.jsx
@@ -1,125 +1,82 @@
-import React, { useState } from "react";
+import React, { useState } from 'react';
 
 //internal import
-import "./styles.css";
+import './styles.css';
 
 function Register({ formRef }) {
-  const [submit, setSubmit] = useState(false);
+	const [submit, setSubmit] = useState(false);
 
-  const handleSubmit = () => {
-    setSubmit((currentSubmit) => {
-      return !currentSubmit;
-    });
-  };
+	const handleSubmit = () => {
+		setSubmit((currentSubmit) => {
+			return !currentSubmit;
+		});
+	};
 
-  return (
-    <>
-      <div className="register-form" id="register" ref={formRef}>
-        <h2>Kindly fill out this form to get registered on our site.</h2>
+	return (
+		<>
+			<div className="register-form" id="register" ref={formRef}>
+				<h2>Kindly fill out this form to get registered on our site.</h2>
 
-        <form className="form">
-          <fieldset>
-            <legend>Personal Information</legend>
-            <label htmlFor="firstName">
-              First Name:{" "}
-              <input
-                type="text"
-                id="firstName"
-                required
-                placeholder="e.g. Dani"
-              />
-            </label>
-
-            <label htmlFor="lastName">
-              Second Name:{" "}
-              <input
-                type="text"
-                id="LastName"
-                required
-                placeholder="e.g. Shitu"
-              />
-            </label>
-
-            <label htmlFor="dob">
-              Date of Birth:{" "}
-              <input type="date" id="dob" required placeholder=" dd/mm/yyyy" />
-            </label>
-
-            <label htmlFor="age">
-              Age:{" "}
-              <input type="number" id="age" required placeholder="e.g. 8" />
-            </label>
-
-            <div className="gender">
-              <p>Gender:</p>
-
-              <label htmlFor="male">
-                <span>Male</span>
-                <input
-                  type="radio"
-                  id="male"
-                  name="gender"
-                  value="Male"
-                  required
-                />
-              </label>
-
-              <label htmlFor="female">
-                <span>Female</span>
-                <input
-                  type="radio"
-                  id="female"
-                  name="gender"
-                  value="Female"
-                  required
-                />
-              </label>
-            </div>
-
-            <label htmlFor="phone">
-              Phone:
-              <input
-                type="tel"
-                id="phone"
-                name="phone"
-                placeholder="e.g. 08012345678"
-                required
-              />
-            </label>
-
-            <label htmlFor="country">
-              Country: <input type="text" id="country" required />
-            </label>
-
-            <label htmlFor="email">
-              Email:{" "}
-              <input
-                type="email"
-                id="email"
-                placeholder="example@gmail.com"
-                required
-              />
-            </label>
-
-            <label htmlFor="password">
-              Password: <input type="password" id="password" required />
-            </label>
-
-            <label htmlFor="confirmPassword">
-              Confirm Password:{" "}
-              <input type="password" id="confirmPassword" required />
-            </label>
-
-            <button type="submit" onClick={handleSubmit}>
-              Submit
-            </button>
-
-            {submit && <p>Form submitted successfully!</p>}
-          </fieldset>
-        </form>
-      </div>
-    </>
-  );
+				<form className="form">
+					<fieldset>
+						<legend>Personal Information</legend>
+						<section>
+							<label htmlFor="firstName" id="firstName">
+								First Name
+							</label>
+							<input type="text" id="firstName" required placeholder="e.g. Dani" />
+						</section>
+						<section>
+							<label htmlFor="lastName" id="lastName">
+								Second Name
+							</label>
+							<input type="text" id="LastName" required placeholder="e.g. Shitu" />
+						</section>
+						<section>
+							<label htmlFor="dob">Date of Birth</label>
+							<input type="date" id="dob" required placeholder=" dd/mm/yyyy" />
+						</section>
+						<section>
+							<label htmlFor="age">Age</label>
+							<input type="number" id="age" required placeholder="e.g. 8" />
+						</section>
+						<div className="gender-options">
+							<p>Gender</p>
+							<div className="gender-selection">
+								<section>
+									<label htmlFor="male">
+										<span>Male</span>
+									</label>
+									<input type="radio" id="male" name="gender" value="Male" required />
+								</section>
+								<section>
+									<label htmlFor="female">
+										<span>Female</span>
+									</label>
+									<input type="radio" id="female" name="gender" value="Female" required />
+								</section>
+							</div>
+						</div>
+						<section>
+							<label htmlFor="email">Email</label>
+							<input type="email" id="email" placeholder="example@gmail.com" required />
+						</section>
+						<section>
+							<label htmlFor="password">Password</label>
+							<input type="password" id="password" required />
+						</section>
+						<section>
+							<label htmlFor="confirmPassword">Confirm Password</label>
+							<input type="password" id="confirmPassword" required />
+						</section>
+						<button type="submit" onClick={handleSubmit}>
+							Submit
+						</button>
+					</fieldset>
+				</form>
+			</div>
+		</>
+	);
 }
 
 export default Register;
